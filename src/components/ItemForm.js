eeb0528863ec83ae3c7ce26ad9a9ca68022fa477
Ej Myles <ejelymyles@gmail.com>
2023-07-31 23:40:20 -0700

    complete CRUD udpates

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,11 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-function ItemForm() {
+function ItemForm({onAddItem}) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+   //use useEffect to write a POST request to add submitted form items (with correct data key/value pairs)
+
+  function handleSubmit(e){
+    e.preventDefault();
+    const itemData = {
+      name: name,
+      category: category,
+      isInCart: false,
+    }; 
+   fetch("http://localhost:4000/items", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(itemData),
+   })
+   .then((r) => r.json())
+   .then((newItem) => onAddItem(newItem));
+  }
+
+
+
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
         <input
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -6,6 +6,38 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+
+
+//useEffect to make a Get request to data, to display items whenc component is rendered
+
+useEffect(() => {
+  fetch("http://localhost:4000/items")
+  .then((r) => r.json())
+  .then((items) => setItems(items));
+}, []);
+
+//add callback to pass to itemForm so that when a new form is submitted, that data can be shared with Shoppinglist
+function handleAddItem(newItem){
+  setItems([...items, newItem]);
+}
+
+// add callback to pass to Item component so that when an item isInCart or !isInCart, that info can be shared with ShoppingList
+function handleUpdateItem(updatedItem){
+  const updatedItems = items.map((item) => {
+    if(item.id === updatedItem.id) {
+      return updatedItem;
+    } else {
+      return item;
+    }
+  })
+  setItems(updatedItems)
+}
+
+// add a callback to pass to the item component so that when the delete button is clicked, that info can be shared with ShoppingList
+function handleDeleteItem(deletedItem){
+  const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+  setItems(updatedItems);
+}
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
@@ -19,14 +51,14 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem={handleAddItem}/>
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
-      <ul className="Items">
+      <ul className="Items" >
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} onUpdateItem={handleUpdateItem} onDeleteItem={handleDeleteItem} />
         ))}
       </ul>
     </div>
